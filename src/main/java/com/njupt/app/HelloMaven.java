e9b64073e9d1d3009858641c80a04930ed76cea4
yingyazhang <992776204@qq.com>
2016-12-24 18:56:16 +0800

    run "merry happy"

---
+++ src/main/java/com/njupt/app/HelloMaven.java
@@ -0,0 +1,11 @@
+package com.njupt.app;
+
+/**
+ * Created by ZYY on 2016/12/24.
+ */
+public class HelloMaven {
+    public static void main(String []args){
+        System.out.println("merry ");
+        System.out.println("happy");
+    }
+}
